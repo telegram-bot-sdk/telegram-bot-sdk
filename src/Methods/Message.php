b6499ac75748be80555c82d89db215b1c282744a
Jonathan Williamson <jonnywilliamson@gmail.com>
2020-04-24 10:47:43 +0100

    Fix spaces.

--- src/Methods/Message.php
+++ src/Methods/Message.php
@@ -395,21 +395,21 @@ trait Message
      *
      * <code>
      * $params = [
-*       'chat_id'                 => '',  // int|string - Required. Unique identifier for the target chat or username of the target channel (in the format "@channelusername"). A native poll can't be sent to a private chat.
-*       'question'                => '',  // string     - Required. Poll question, 1-255 characters
-*       'options'                 => '',  // array      - Required. List of answer options, 2-10 strings 1-100 characters each
-*       'is_anonymous'            => '',  // bool       - (Optional). True, if the poll needs to be anonymous, defaults to True
-*       'type'                    => '',  // string     - (Optional). Poll type, “quiz” or “regular”, defaults to “regular”
-*       'allows_multiple_answers' => '',  // bool       - (Optional). True, if the poll allows multiple answers, ignored for polls in quiz mode, defaults to False
-*       'correct_option_id'       => '',  // int        - (Optional). 0-based identifier of the correct answer option, required for polls in quiz mode
-*       'explanation'             => '',  // string     - (Optional). Text that is shown when a user chooses an incorrect answer or taps on the lamp icon in a quiz-style poll, 0-200 characters with at most 2 line feeds after entities parsing
-*       'explanation_parse_mode'  => '',  // string     - (Optional). Mode for parsing entities in the explanation. See formatting options for more details.
-*       'open_period'             => '',  // int        - (Optional). Amount of time in seconds the poll will be active after creation, 5-600. Can't be used together with close_date.
-*       'close_date'              => '',  // int        - (Optional). Point in time (Unix timestamp) when the poll will be automatically closed. Must be at least 5 and no more than 600 seconds in the future. Can't be used together with open_period.
-*       'is_closed'               => '',  // bool       - (Optional). Pass True, if the poll needs to be immediately closed. This can be useful for poll preview.
-*       'disable_notification'    => '',  // bool       - (Optional). Sends the message silently. Users will receive a notification with no sound.
-*       'reply_to_message_id'     => '',  // int        - (Optional). If the message is a reply, ID of the original message
-*       'reply_markup'            => '',  // string     - (Optional). Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.
+     *       'chat_id'                 => '',  // int|string - Required. Unique identifier for the target chat or username of the target channel (in the format "@channelusername"). A native poll can't be sent to a private chat.
+     *       'question'                => '',  // string     - Required. Poll question, 1-255 characters
+     *       'options'                 => '',  // array      - Required. List of answer options, 2-10 strings 1-100 characters each
+     *       'is_anonymous'            => '',  // bool       - (Optional). True, if the poll needs to be anonymous, defaults to True
+     *       'type'                    => '',  // string     - (Optional). Poll type, “quiz” or “regular”, defaults to “regular”
+     *       'allows_multiple_answers' => '',  // bool       - (Optional). True, if the poll allows multiple answers, ignored for polls in quiz mode, defaults to False
+     *       'correct_option_id'       => '',  // int        - (Optional). 0-based identifier of the correct answer option, required for polls in quiz mode
+     *       'explanation'             => '',  // string     - (Optional). Text that is shown when a user chooses an incorrect answer or taps on the lamp icon in a quiz-style poll, 0-200 characters with at most 2 line feeds after entities parsing
+     *       'explanation_parse_mode'  => '',  // string     - (Optional). Mode for parsing entities in the explanation. See formatting options for more details.
+     *       'open_period'             => '',  // int        - (Optional). Amount of time in seconds the poll will be active after creation, 5-600. Can't be used together with close_date.
+     *       'close_date'              => '',  // int        - (Optional). Point in time (Unix timestamp) when the poll will be automatically closed. Must be at least 5 and no more than 600 seconds in the future. Can't be used together with open_period.
+     *       'is_closed'               => '',  // bool       - (Optional). Pass True, if the poll needs to be immediately closed. This can be useful for poll preview.
+     *       'disable_notification'    => '',  // bool       - (Optional). Sends the message silently. Users will receive a notification with no sound.
+     *       'reply_to_message_id'     => '',  // int        - (Optional). If the message is a reply, ID of the original message
+     *       'reply_markup'            => '',  // string     - (Optional). Additional interface options. A JSON-serialized object for an inline keyboard, custom reply keyboard, instructions to remove reply keyboard or to force a reply from the user.
      * ]
      * </code>
      *
