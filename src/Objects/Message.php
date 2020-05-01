3017cd723237cc3d74ed4283327d3f2a20b9d1e0
Jonathan Williamson <jonnywilliamson@gmail.com>
2020-05-02 00:33:56 +0100

    Updated all message types

--- src/Objects/Message.php
+++ src/Objects/Message.php
@@ -121,19 +121,37 @@ class Message extends BaseObject
      */
     public function detectType(): ?string
     {
-         $types = array_merge(
-            array_keys($this->relations()),
-            [
-                "text",
-                "new_chat_title",
-                "delete_chat_photo",
-                "group_chat_created",
-                "supergroup_chat_created",
-                "channel_chat_created",
-                "migrate_to_chat_id",
-                "migrate_from_chat_id",
-            ]
-        );
+        $types = [
+            'text',
+            'audio',
+            'document',
+            'animation',
+            'game',
+            'photo',
+            'sticker',
+            'video',
+            'voice',
+            'video_note',
+            'contact',
+            'location',
+            'venue',
+            'poll',
+            'dice',
+            'new_chat_members',
+            'left_chat_member',
+            'new_chat_title',
+            'new_chat_photo',
+            'delete_chat_photo',
+            'group_chat_created',
+            'supergroup_chat_created',
+            'channel_chat_created',
+            'migrate_to_chat_id',
+            'migrate_from_chat_id',
+            'pinned_message',
+            'invoice',
+            'successful_payment',
+            'passport_data',
+        ];
 
         return $this->collect()
             ->keys()
