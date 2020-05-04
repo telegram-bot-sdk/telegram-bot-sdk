1ae38a6ee5df83486b3d290ea2db272f4e9ee6f4
Irfaq Syed <syed@lukonet.com>
2020-05-05 03:36:00 +0530

    Remove getEventName and use object type to fire events.

--- src/Objects/Update.php
+++ src/Objects/Update.php
@@ -71,11 +71,6 @@ class Update extends BaseObject
         return $this->{$this->objectType()};
     }
 
-    public function getEventName(): string
-    {
-        return $this->objectType();
-    }
-
     /**
      * Get chat object (if exists).
      */
