f8960726769ff83544869a755830893c89c0767d
Jonathan Williamson <jonnywilliamson@gmail.com>
2020-05-05 22:21:51 +0100

    Fix docbloc parsing incorrectly.

--- src/Methods/Query.php
+++ src/Methods/Query.php
@@ -26,7 +26,7 @@ trait Query
      *       'callback_query_id'  => '',  // string - Required. Unique identifier for the query to be answered
      *       'text'               => '',  // string - (Optional). Text of the notification. If not specified, nothing will be shown to the user, 0-200 characters
      *       'show_alert'         => '',  // bool   - (Optional). If true, an alert will be shown by the client instead of a notification at the top of the chat screen. Defaults to false.
-     *       'url'                => '',  // string - (Optional). URL that will be opened by the user's client. If you have created a Game and accepted the conditions via @Botfather, specify the URL that opens your game – note that this will only work if the query comes from a callback_game button. Otherwise, you may use links like telegram.me/your_bot?start=XXXX that open your bot with a parameter.
+     *       'url'                => '',  // string - (Optional). URL that will be opened by the user's client. If you have created a Game and accepted the conditions via "@Botfather", specify the URL that opens your game – note that this will only work if the query comes from a callback_game button. Otherwise, you may use links like telegram.me/your_bot?start=XXXX that open your bot with a parameter.
      *       'cache_time'         => '',  // int    - (Optional). The maximum amount of time in seconds that the result of the callback query may be cached client-side. Telegram apps will support caching starting in version 3.14. Defaults to 0.
      * ]
      * </code>
