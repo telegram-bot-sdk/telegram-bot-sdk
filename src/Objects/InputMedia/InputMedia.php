38975da8ba6eb1606ada5c27811608aff66e3915
Jonathan Williamson <jonnywilliamson@gmail.com>
2020-05-06 19:14:24 +0100

    Fix sendMediaGroup method and create proper mediaGroupItems using new base object

--- src/Objects/InputMedia/InputMedia.php
+++ src/Objects/InputMedia/InputMedia.php
@@ -2,12 +2,19 @@
 
 namespace Telegram\Bot\Objects\InputMedia;
 
-use Telegram\Bot\Objects\BaseObject;
+use Telegram\Bot\Objects\BaseCreateObject;
 
 /**
  * Class InputMedia.
- *  This object represents the content of a media message to be sent.
+ *
+ * This object represents the content of a media message to be sent.
  */
-class InputMedia extends BaseObject
+abstract class InputMedia extends BaseCreateObject
 {
+    protected static string $type;
+
+    public static function make($data = []): self
+    {
+        return new static(array_merge($data, ['type' => static::$type]));
+    }
 }
--- src/Objects/InputMedia/InputMediaAudio.php
+++ src/Objects/InputMedia/InputMediaAudio.php
@@ -6,23 +6,20 @@ use Telegram\Bot\FileUpload\InputFile;
 
 /**
  * Class InputMediaAudio.
+ *
  * Represents an audio file to be treated as music to be sent.
  *
- * @property string           $type         Type of the result, must be audio.
- * @property string           $media        File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name.
- * @property InputFile|string $thumb        (Optional). Thumbnail of the file sent. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail‘s width and height should not exceed 90. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can’t be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>
- * @property string           $caption      (Optional). Caption of the audio to be sent, 0-200 characters
- * @property string           $parse_mode   (Optional). Send Markdown or HTML, if you want Telegram apps to show bold, italic, fixed-width text or inline URLs in the media caption.
- * @property int              $duration     (Optional). Duration of the audio in seconds
- * @property string           $performer    (Optional). Performer of the audio
- * @property string           $title        (Optional). Title of the audio
+ * @link https://core.telegram.org/bots/api#inputmediaaudio
+ *
+ * @method $this media($string)             Required. File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name. More info on Sending Files »
+ * @method $this thumb($inputFileOrString)  (Optional). Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail‘s width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can’t be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
+ * @method $this caption($string)           (Optional). Caption of the audio to be sent, 0-1024 characters after entities parsing
+ * @method $this parseMode($string)         (Optional). Mode for parsing entities in the audio caption
+ * @method $this duration($int)             (Optional). Duration of the audio in seconds
+ * @method $this performer($string)         (Optional). Performer of the audio
+ * @method $this title($string)             (Optional). Title of the audio
  */
 class InputMediaAudio extends InputMedia
 {
-    public function relations(): array
-    {
-        return [
-            'thumb' => InputFile::class,
-        ];
-    }
+    protected static string $type = 'audio';
 }
