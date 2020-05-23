6a819631d2ba7ff1801bd4a574a39eddd281f4ec
Irfaq Syed <syed@lukonet.com>
2020-05-24 01:18:41 +0530

    Revise thumbnail docs as per official changes.

--- src/Objects/InputMedia/InputMediaAnimation.php
+++ src/Objects/InputMedia/InputMediaAnimation.php
@@ -8,7 +8,7 @@ namespace Telegram\Bot\Objects\InputMedia;
  * @link https://core.telegram.org/bots/api#inputmediaanimation
  *
  * @method $this media($string)             Required. File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name.
- * @method $this thumb($inputFileOrString)  (Optional). Thumbnail of the file sent. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail‘s width and height should not exceed 90. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can’t be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>
+ * @method $this thumb($inputFileOrString)  (Optional). Thumbnail can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Thumbnails can't be reused and can be only uploaded as a new file.
  * @method $this caption($string)           (Optional). Caption of the animation to be sent, 0-200 characters
  * @method $this parseMode($string)         (Optional). Send Markdown or HTML, if you want Telegram apps to show bold, italic, fixed-width text or inline URLs in the media caption.
  * @method $this width($int)                (Optional). Animation width
--- src/Objects/InputMedia/InputMediaAudio.php
+++ src/Objects/InputMedia/InputMediaAudio.php
@@ -10,7 +10,7 @@ namespace Telegram\Bot\Objects\InputMedia;
  * @link https://core.telegram.org/bots/api#inputmediaaudio
  *
  * @method $this media($string)             Required. File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass “attach://<file_attach_name>” to upload a new one using multipart/form-data under <file_attach_name> name. More info on Sending Files »
- * @method $this thumb($inputFileOrString)  (Optional). Thumbnail of the file sent can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail‘s width and height should not exceed 320. Ignored if the file is not uploaded using multipart/form-data. Thumbnails can’t be reused and can be only uploaded as a new file, so you can pass “attach://<file_attach_name>” if the thumbnail was uploaded using multipart/form-data under <file_attach_name>.
+ * @method $this thumb($inputFileOrString)  (Optional). Thumbnail can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Thumbnails can't be reused and can be only uploaded as a new file.
  * @method $this caption($string)           (Optional). Caption of the audio to be sent, 0-1024 characters after entities parsing
  * @method $this parseMode($string)         (Optional). Mode for parsing entities in the audio caption
  * @method $this duration($int)             (Optional). Duration of the audio in seconds
