c4dfffe2ae52e2d5253f1adcc0bc49798c98abb9
Jonathan Williamson <jonnywilliamson@gmail.com>
2023-05-07 09:34:43 +0100

    Add tests to give 100% coverage

--- tests/Unit/Objects/ResponseObject.php
+++ tests/Unit/Objects/ResponseObject.php
@@ -10,6 +10,13 @@ it('can add custom data to the response object', function () {
     expect($response->jsonSerialize())->toBe(['foo' => 'bar', 'custom_data' => ['baz' => 'qux']]);
 });
 
+it('can add custom data without a key', function () {
+    $response = new ResponseObject(['custom_data' => ['foo' => 'bar']]);
+    $response = $response->withCustomData(null, 'baz');
+
+    expect($response->getCustomData())->toBe(['foo' => 'bar', 'baz']);
+});
+
 it('can get custom data from the response object', function () {
     $response = new ResponseObject(['custom_data' => ['foo' => 'bar']]);
 
@@ -21,6 +28,12 @@ it('throws an exception when trying to set an offset', function () {
 
     $response['baz'] = 'qux';
 })->expectException(LogicException::class);
+
+it('throws an exception when trying to use __set', function () {
+    $response = new ResponseObject(['foo' => 'bar']);
+    $response->__set('baz', 'qux');
+})
+    ->expectException(LogicException::class);
 
 it('can count the number of fields in the response object', function () {
     $response = new ResponseObject(['foo' => 'bar', 'baz' => 'qux']);
@@ -64,8 +77,20 @@ it('can check if a field exists using array access', function () {
     expect(isset($response['foo']))->toBeTrue();
 });
 
+it('can check if a field exists using __isset', function () {
+    $response = new ResponseObject(['foo' => 'bar']);
+
+    expect($response->__isset('foo'))->toBeTrue();
+});
+
 it('throws an exception when trying to unset a field using array access', function () {
     $response = new ResponseObject(['foo' => 'bar']);
 
     unset($response['foo']);
+})->expectException(LogicException::class);
+
+it('throws an exception when trying to unset a field using __unset', function () {
+    $response = new ResponseObject(['foo' => 'bar']);
+
+    $response->__unset('foo');
 })->expectException(LogicException::class);
