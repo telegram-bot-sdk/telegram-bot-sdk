d5514fc4459a464b4bcc98438795e8490a59da72
Irfaq Syed <irazasyed@users.noreply.github.com>
2023-05-11 15:00:31 +0530

    Add BotFake to mock test

---
+++ src/Testing/BotFake.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Telegram\Bot\Testing;
+
+use PHPUnit\Framework\Assert as PHPUnit;
+use Exception;
+use Throwable;
+use Telegram\Bot\Testing\Requests\TestRequest;
+
+class BotFake
+{
+    /**
+     * @var array<array-key, TestRequest>
+     */
+    private array $requests = [];
+
+    /**
+     * @param  array<array-key, string>  $responses
+     */
+    public function __construct(protected array $responses = [])
+    {
+    }
+
+    public function addResponses(array $responses): void
+    {
+        $this->responses = [...$this->responses, ...$responses];
+    }
+
+    public function assertSent(string $method, $callback = null): void
+    {
+        if (is_int($callback)) {
+            $this->assertSentTimes($method, $callback);
+
+            return;
+        }
+
+        PHPUnit::assertNotSame(
+            $this->sent($method, $callback), [], "The expected [{$method}] request was not sent."
+        );
+    }
+
+    protected function assertSentTimes(string $method, int $times = 1): void
+    {
+        $count = count($this->sent($method));
+
+        PHPUnit::assertSame(
+            $times, $count,
+            "The expected [{$method}] method was sent {$count} times instead of {$times} times."
+        );
+    }
+
+    protected function sent(string $method, callable $callback = null): array
+    {
+        if (! $this->hasSent($method)) {
+            return [];
+        }
+
+        $callback = $callback ?: fn (): bool => true;
+
+        return array_filter($this->methodsOf($method), fn (TestRequest $request) => $callback($request->parameters()));
+    }
+
+    protected function hasSent(string $method): bool
+    {
+        return $this->methodsOf($method) !== [];
+    }
+
+    public function assertNotSent(string $method, callable $callback = null): void
+    {
+        PHPUnit::assertCount(
+            0, $this->sent($method, $callback),
+            "The unexpected [{$method}] request was sent."
+        );
+    }
+
+    public function assertNothingSent(): void
+    {
+        $methodNames = implode(
+            separator: ', ',
+            array: array_map(fn (TestRequest $request): string => $request->method(), $this->requests)
+        );
+
+        PHPUnit::assertEmpty($this->requests, 'The following requests were sent unexpectedly: '.$methodNames);
+    }
+
+    /**
+     * @return array<array-key, TestRequest>
+     */
+    protected function methodsOf(string $method): array
+    {
+        return array_filter($this->requests, fn (TestRequest $request): bool => $request->method() === $method);
+    }
+
+    public function record(TestRequest $request): mixed
+    {
+        $this->requests[] = $request;
+
+        $response = array_shift($this->responses);
+
+        if (is_null($response)) {
+            throw new Exception('No fake responses left.');
+        }
+
+        if ($response instanceof Throwable) {
+            throw $response;
+        }
+
+        return $response;
+    }
+
+    public function __call(string $method, array $parameters)
+    {
+        return $this->record(new TestRequest($method, ...$parameters));
+    }
+}
---
+++ src/Testing/Requests/TestRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Telegram\Bot\Testing\Requests;
+
+final class TestRequest
+{
+    /**
+     * @param  array<string, mixed>|string|null  $parameters
+     */
+    public function __construct(protected string $method, protected array|string|null $parameters)
+    {
+    }
+
+    public function method(): string
+    {
+        return $this->method;
+    }
+
+    /**
+     * @return array<string, mixed>|string|null
+     */
+    public function parameters(): array|string|null
+    {
+        return $this->parameters;
+    }
+}
