8ab833081a310b26fb676ebd5f2e5f1bf736f2ce
Irfaq Syed <irazasyed@users.noreply.github.com>
2023-05-13 06:54:03 +0530

    BotManager tests

--- tests/Unit/BotManagerTest.php
+++ tests/Unit/BotManagerTest.php
@@ -4,95 +4,231 @@ use Telegram\Bot\Bot;
 use Telegram\Bot\BotManager;
 use Telegram\Bot\Exceptions\TelegramSDKException;
 
-it('returns the correct default bot name', function () {
-    $config = ['use' => 'test_bot'];
-    $manager = new BotManager($config);
-
-    expect($manager->getDefaultBotName())->toBe('test_bot');
-});
-
-it('can set a default bot name', function () {
+it('can create BotManager instance', function () {
     $config = [
-        'use' => 'test_bot',
+        'use'  => 'default',
         'bots' => [
-            'new_bot' => [
-                'token' => 123,
-            ],
-        ],
-    ];
-    $manager = new BotManager($config);
-    $manager->setDefaultBotName('new_bot');
-
-    expect($manager->getDefaultBotName())->toBe('new_bot');
-});
-
-it('throws an exception if a bot does not have any config settings', function () {
-    $config = [
-        'use' => 'test_bot',
-        'bots' => [],
-    ];
-    $manager = new BotManager($config);
-    $manager->setDefaultBotName('new_bot');
-})
-    ->expectException(TelegramSDKException::class);
-
-it('allows a bot to reconnect', function () {
-    $config = [
-        'use' => 'test_bot',
-        'bots' => [
-            'test_bot' => [
-                'token' => 123,
+            'default' => [
+                'token' => 'your-bot-token',
             ],
         ],
     ];
 
-    $manager = new BotManager($config);
-    $bot = $manager->reconnect('test_bot');
+    $botManager = new BotManager($config);
 
-    expect($bot)->toBeInstanceOf(Bot::class)
-        ->and($bot->getName())->toBe('test_bot');
-
-    expect($manager->getDefaultBotName())->toBe('test_bot');
+    expect($botManager)->toBeInstanceOf(BotManager::class);
 });
 
-it('can disconnect a bot', function () {
+it('can get all the created bots', function () {
     $config = [
-        'use' => 'test_bot',
+        'use'  => 'default',
         'bots' => [
-            'test_bot' => [
-                'token' => 123,
+            'default' => [
+                'token' => 'your-bot-token',
+            ],
+            'second'  => [
+                'token' => 'your-second-bot-token',
             ],
         ],
     ];
 
-    $manager = new BotManager($config);
-    $manager->reconnect('test_bot');
-    expect($manager->getBots())->toHaveKey('test_bot');
+    $botManager = new BotManager($config);
 
-    $manager->disconnect('test_bot');
-    expect($manager->getBots())->not()->toHaveKey('test_bot');
+    expect($botManager->getBots())->toHaveCount(0);
+
+    $botManager->bot('default');
+
+    expect($botManager->getBots())->toHaveCount(1);
+
+    $botManager->bot('second');
+
+    expect($botManager->getBots())->toHaveCount(2);
 });
 
-it('can get a bot config', function () {
+it('can get the default bot name', function () {
     $config = [
-        'use' => 'test_bot',
+        'use'  => 'default',
         'bots' => [
-            'test_bot' => [
-                'token' => 123,
+            'default' => [
+                'token' => 'your-bot-token',
             ],
         ],
     ];
 
-    $manager = new BotManager($config);
-    expect($manager->getBotConfig())->toMatchArray(
-        [
-            'token' => 123,
-            'bot' => 'test_bot',
-            'global' => [
-                'use' => 'test_bot',
-                'bots' => [
-                    'test_bot' => ['token' => 123],
-                ],
+    $botManager = new BotManager($config);
+
+    expect($botManager->getDefaultBotName())->toBe('default');
+});
+
+it('can set the default bot name', function () {
+    $config = [
+        'use'  => 'default',
+        'bots' => [
+            'default' => [
+                'token' => 'your-bot-token',
             ],
-        ]);
+            'second'  => [
+                'token' => 'your-second-bot-token',
+            ],
+        ],
+    ];
+
+    $botManager = new BotManager($config);
+
+    $botManager->setDefaultBotName('second');
+
+    expect($botManager->getDefaultBotName())->toBe('second');
+});
+
+it('can get a bot instance', function () {
+    $config = [
+        'use'  => 'default',
+        'bots' => [
+            'default' => [
+                'token' => 'your-bot-token',
+            ],
+            'second'  => [
+                'token' => 'your-second-bot-token',
+            ],
+        ],
+    ];
+
+    $botManager = new BotManager($config);
+
+    $defaultBot = $botManager->bot('default');
+    $secondBot = $botManager->bot('second');
+
+    expect($defaultBot)->toBeInstanceOf(Bot::class)
+        ->and($secondBot)->toBeInstanceOf(Bot::class);
+});
+
+it('can reconnect to a bot', function () {
+    $config = [
+        'use'  => 'default',
+        'bots' => [
+            'default' => [
+                'token' => 'your-bot-token',
+            ],
+            'second'  => [
+                'token' => 'your-second-bot-token',
+            ],
+        ],
+    ];
+
+    $botManager = new BotManager($config);
+
+    $defaultBot = $botManager->bot('default');
+    $secondBot = $botManager->bot('second');
+
+    expect($defaultBot)->toBeInstanceOf(Bot::class)
+        ->and($secondBot)->toBeInstanceOf(Bot::class);
+
+    $newDefaultBot = $botManager->reconnect('default');
+    $newSecondBot = $botManager->reconnect('second');
+
+    expect($newDefaultBot)->toBeInstanceOf(Bot::class)
+        ->and($newSecondBot)->toBeInstanceOf(Bot::class)
+        ->and($newDefaultBot)->not->toBe($defaultBot)
+        ->and($newSecondBot)->not->toBe($secondBot);
+});
+
+it('can disconnect from a bot', function () {
+    $config = [
+        'use' => 'default',
+        'bots' => [
+            'default' => [
+                'token' => 'your-bot-token',
+            ],
+            'second' => [
+                'token' => 'your-second-bot-token',
+            ],
+        ],
+    ];
+
+    $botManager = new BotManager($config);
+
+    $botManager->bot('default');
+    $botManager->bot('second');
+
+    expect($botManager->getBots())->toHaveCount(2);
+
+    $botManager->disconnect('default');
+
+    expect($botManager->getBots())->toHaveCount(1)
+        ->and($botManager->getBots())->not->toHaveKey('default');
+});
+
+it('can get the configuration for a bot', function () {
+    $config = [
+        'use'  => 'default',
+        'bots' => [
+            'default' => [
+                'token' => 'your-bot-token',
+            ],
+            'second' => [
+                'token' => 'your-second-bot-token',
+            ],
+        ],
+    ];
+    $botManager = new BotManager($config);
+
+    $botConfig = $botManager->getBotConfig('second');
+
+    expect($botConfig)->toBeArray()
+        ->and($botConfig)->toHaveKey('bot')
+        ->and($botConfig)->toHaveKey('global')
+        ->and($botConfig)->toHaveKey('token')
+        ->and($botConfig['bot'])->toBe('second')
+        ->and($botConfig['token'])->toBe('your-second-bot-token');
+});
+
+it('can get the configuration for a specific bot', function () {
+    $config = [
+        'use'  => 'default',
+        'bots' => [
+            'default' => [
+                'token' => 'your-bot-token',
+            ],
+        ],
+    ];
+    $botManager = new BotManager($config);
+
+    $botConfig = $botManager->getBotConfig();
+
+    expect($botConfig)->toBeArray()
+        ->and($botConfig)->toHaveKey('bot')
+        ->and($botConfig)->toHaveKey('global');
+});
+
+it('throws an exception when getting the configuration for a non-configured bot', function () {
+    $config = [
+        'use' => 'default',
+        'bots' => [
+            'default' => [
+                'token' => 'your-bot-token',
+            ],
+        ],
+    ];
+
+    $this->expectExceptionMessage("Bot [non-existing-bot] not configured.");
+
+    $botManager = new BotManager($config);
+    $botManager->getBotConfig('non-existing-bot');
+})->expectException(TelegramSDKException::class);
+
+it('can forward method calls to the default bot', function () {
+    $config = [
+        'use' => 'default',
+        'bots' => [
+            'default' => [
+                'token' => 'your-bot-token',
+            ],
+        ],
+    ];
+
+    $botManager = new BotManager($config);
+
+    $result = $botManager->getName();
+
+    expect($result)->toBe('default');
 });
